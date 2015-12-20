ec611bee82d9348b447e991f67cbcdea214ef05e
Dende <dende76@gmx.de>
2015-12-20 18:41:39 +0100

    changes

--- src/fermat/FermatZahl.java
+++ src/fermat/FermatZahl.java
@@ -2,9 +2,24 @@ package fermat;
 
 public class FermatZahl {
 
-	public static double getFermatZahl(double a){
-		
-		a = Math.pow(2, Math.pow(2, a))+1;
-		return a;
+	private double x = 0;
+
+	public double getFermatZahl(double a) {
+
+		if (x == 0) {
+			x = pow(a);
+			a = x;
+		}
+
+		return a = 2 * pow(a - 1) + 1;
+	}
+
+	private double pow(double a) {
+
+		if (a == 0) {
+			return 1;
+		}
+
+		return a = 2 * pow(a - 1);
 	}
 }
--- src/ggT/GGT.java
+++ src/ggT/GGT.java
@@ -1,21 +1,26 @@
 package ggT;
 
+import java.math.BigInteger;
+
+import fermat.FermatZahl;
+
 public class GGT{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		double f = fermat.FermatZahl.getFermatZahl(IO.readInt("Bitte Fermat-Zahl 1 angeben: F"));
-		double g = fermat.FermatZahl.getFermatZahl(IO.readInt("Bitte Fermat-Zahl 1 angeben: F"));
-		System.out.println(f);
-		System.out.println(g);
+		FermatZahl f = new FermatZahl();
+		FermatZahl g = new FermatZahl();
+		double a = f.getFermatZahl(IO.readInt("Bitte Fermat-Zahl 1 angeben: F"));
+		double b = g.getFermatZahl(IO.readInt("Bitte Fermat-Zahl 1 angeben: F"));
+		System.out.println(a);
+		System.out.println(b);
 		
 //		double a = IO.readInt("Bitte a eingeben: ");
 //		double b = IO.readInt("Bitte b eingeben: ");
 		
 		RekursiveAlgorithmus ggt = new RekursiveAlgorithmus();
-		System.out.println("Der ggT von " + f + " und " + g + " ist: " + ggt.getGGT(f, g));
+		System.out.println("Der ggT von " + a + " und " + b + " ist: " + ggt.getGGT(a, b));
 		
 		
 		
